a63d5f9bc9e93c87261fb74b29c9d0f16f472d48
Maxime Condaminat <me@maximecd.dev>
2023-09-16 13:45:16 +0200

    dummies update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'maximecd - labs',
+  title: 'labs | maximecd',
   description: 'React experiments by Maxime Condaminat',
 }
 
